74388d46710601bc6a866f0fcb51202e9cbbfe11
adrikoz <adrikozh@gmail.com>
2020-05-21 11:48:40 +0100

    testing branching

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fundder',
+      title: 'Fundder Test',
       theme: ThemeData(
         // This is the theme of your application.
         //
