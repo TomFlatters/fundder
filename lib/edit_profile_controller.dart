0132c82e80c911d250cabd51408f5766bf890c7b
adrikoz <adrikozh@gmail.com>
2020-06-10 16:16:28 +0100

    added edit profile screen

--- lib/edit_profile_controller.dart
+++ lib/edit_profile_controller.dart
@@ -9,17 +9,17 @@ class EditProfile extends StatefulWidget {
 
 class _EditProfileState extends State<EditProfile> {
 
-  final List<String> whoDoes = <String>["A specific person",'Myself','Anyone'];
-  final List<String> charities = <String>["Cancer Research",'British Heart Foundation','Oxfam'];
+  final List<String> entries = <String>["Name",'Username','Website','Bio','Email','Phone','Gender'];
+  final List<String> hints = <String>["Name",'Username','Website','Something about you','Email','Phone','Gender'];
   int selected = -1;
   int charity = -1;
 
   @override
-  Widget build(BuildContext context) {
+Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Edit Profile"),
+        title: Text('Edit Profile'),
         actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.close), 
@@ -28,24 +28,127 @@ class _EditProfileState extends State<EditProfile> {
         ],
         leading: new Container(),
       ),
-      body:
-        ListView(
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 5),
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, 
-                children: <Widget>[
-                  Text('This is where the user can edit their profile.',style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17,
-                        ),),
-                ])
-            ), 
-          ], 
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 20, bottom:10),
+            alignment: Alignment.center,
+            child: Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image:  new NetworkImage(
+                    "https://i.imgur.com/BoN9kdC.png")
+                )
+              ),
+            ),
+          ), Center(
+            child: GestureDetector(
+              child: Text("Change profile photo",
+              style: TextStyle(
+                color: Colors.blue,
+              ),),
+              onTap: (){_changePic();},
+            ),
           ),
+          ListView.separated(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10.0),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal:20),
+                    width:80,
+                    child: Text(entries[index])
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: hints[index],
+                      ),
+                    ),
+                    )
+                ],
+              );
+            },
+            separatorBuilder: (BuildContext context, int index){
+              return SizedBox(
+                height: 10,
+              );
+            },
+          ),
+        ]
+      ),
     );
+ }
+
+ void _changePic() {
+  showModalBottomSheet(
+    context: context, 
+    builder: (context) {
+      return ChangePic();
+    }
+    );
+  }
+
+}
+
+class ChangePic extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF737373),
+      height: 350,
+      child: Container(
+        child: _buildBottomNavigationMenu(context),
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.only(
+            topLeft: const Radius.circular(10),
+            topRight: const Radius.circular(10),
+            ),
+        ),
+      ),
+    );
+  }
+
+  ListView _buildBottomNavigationMenu(context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: null,
+          title: Text('Remove Current Photo'),
+          onTap: () async {
+            },
+        ),
+        ListTile(
+          leading: null,
+          title: Text('Import from Facebook'),
+          onTap: () {
+            },
+        ),
+                ListTile(
+          leading: null,
+          title: Text('Take Photo'),
+          onTap: () {
+            },
+        ),
+                ListTile(
+          leading: null,
+          title: Text('Choose From Library'),
+          onTap: () {
+            },
+        ),
+      ],
+      );
   }
 }
