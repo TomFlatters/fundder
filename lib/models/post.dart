491adb429835bf8ece6b2ff7f9e5e54fc70d2f77
Tom Flatters <tflats.tf@gmail.com>
2020-06-20 22:06:11 +0100

    Integrate post streams from Firestore

---
+++ lib/models/post.dart
@@ -0,0 +1,22 @@
+
+class Post {
+
+  final String author;
+  final String title;
+  final String charity;
+  final String amountRaised;
+  final String targetAmount;
+  final List likes;
+  final comments;
+  final String subtitle;
+  // final timestamp; 
+
+  Post({ 
+    this.author, this.title, this.charity,
+    this.amountRaised, this.targetAmount, 
+    this.subtitle, this.likes,
+    this.comments
+    // this.timestamp
+    });
+
+}
