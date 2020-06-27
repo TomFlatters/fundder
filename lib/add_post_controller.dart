3f28181923f9281bedf95401db5f7e21d22b9f66
Tom Flatters <tflats.tf@gmail.com>
2020-06-27 22:44:48 +0100

    Add post form working with Firebase Storage

--- lib/add_post_controller.dart
+++ lib/add_post_controller.dart
@@ -1,7 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fundder/services/database.dart';
+import 'package:provider/provider.dart';
+import 'models/user.dart';
 import 'view_post_controller.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:fundder/models/post.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_icons/flutter_icons.dart';
@@ -14,14 +19,10 @@ class Page2 extends StatefulWidget {
 class _Page2State extends State<Page2> {
   int _current = 0;
   CarouselController _carouselController = CarouselController();
-  final List<Widget> screens = [
-   DefineDescription(),
-   ChoosePerson(),
-   SetMoney(),
-   ChooseCharity(),
-   ImageUpload()];
+
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -32,7 +33,43 @@ class _Page2State extends State<Page2> {
                     ? Text('Submit', style: TextStyle(fontWeight: FontWeight.bold))
                     : Text('Next', style: TextStyle(fontWeight: FontWeight.bold)),
             onPressed: _current == 4 
-            ? () {Navigator.of(context).pushReplacement(_viewPost());}
+            ? () 
+            {
+              // add image to firebase storage
+              final String fileLocation = user.uid + "/" + DateTime.now().microsecondsSinceEpoch.toString();
+              DatabaseService(uid: user.uid).uploadImage(imageFile, fileLocation).then(
+                (downloadUrl) => {
+                  print("Successful image upload"),
+                  print(downloadUrl),
+
+                  // create post from the state and image url, and add that post to firebase
+                  DatabaseService(uid: user.uid).uploadPost(
+                    new Post(
+                      title: titleController.text.toString(),
+                      subtitle: subtitleController.text.toString(),
+                      author: user.uid,
+                      charity: charities[charity],
+                      likes: [],
+                      comments: {},
+                      timestamp: DateTime.now(),
+                      amountRaised: "0",
+                      targetAmount: moneyController.text.toString(),
+                      imageUrl: downloadUrl,
+                    )
+                  )
+                  .then((postId) => {
+                    print("The doc id is " + postId.toString()),
+                    
+                    // if the post is successfully added, view the post
+                    DatabaseService(uid: user.uid).getPostById(postId.toString())
+                    .then((post) => {
+                      Navigator.of(context)
+                        .pushReplacement(_viewPost(post))
+                    })
+                  })
+                });
+
+            }
             : () {/*Navigator.of(context).pushReplacement(_viewPost());*/
             _carouselController.nextPage(
             duration: Duration(milliseconds: 300), curve: Curves.linear);},
@@ -60,17 +97,27 @@ class _Page2State extends State<Page2> {
               enlargeCenterPage: false,
               // autoPlay: false,
             ),
-            items: screens,
+            items: [
+                _defineDescription(),
+                _choosePerson(),
+                _setMoney(),
+                _chooseCharity(),
+                _imageUpload()
+                ],
           );
         },
       ),
     );
   }
-}
 
-class DefineDescription extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  // Define widgets for each of the form stages:
+
+  // _defineDescription state:
+  final titleController = TextEditingController();
+  final subtitleController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  Widget _defineDescription() {
     return ListView(
       children: <Widget>[
         Container(
@@ -89,6 +136,7 @@ class DefineDescription extends StatelessWidget {
                     ),),
               ),
               TextField(
+                controller: titleController,
                 decoration: InputDecoration(
                   hintText: 'Write a title'
                 )
@@ -102,6 +150,7 @@ class DefineDescription extends StatelessWidget {
                     ),),
               ),
               TextField(
+                controller: subtitleController,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
@@ -117,6 +166,7 @@ class DefineDescription extends StatelessWidget {
                     ),),
               ),
               TextField(
+                controller: descriptionController,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
@@ -128,70 +178,63 @@ class DefineDescription extends StatelessWidget {
         
       );
   }
-}
 
-class ChoosePerson extends StatefulWidget {
-  @override
-  _ChoosePersonState createState() => _ChoosePersonState();
-}
-
-class _ChoosePersonState extends State<ChoosePerson> {
+  // _choosePerson state:
   int selected = -1;
   final List<String> whoDoes = <String>["A specific person",'Myself','Anyone'];
   final List<String> subWho = <String>["Does not have to be a Fundder user",'Raise money for your own challenge','Will be public and anyone will be able to accept the challenge. This appears in the custom challenges in the do tab in the Feed'];
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 5),
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, 
-                children: <Widget>[
-                  Text('Who do you want to do it',style: TextStyle(
-                          fontFamily: 'Quicksand',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),),
-                  ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    padding: const EdgeInsets.symmetric(vertical: 5.0),
-                    shrinkWrap: true,
-                    itemCount: whoDoes.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        dense: true,
-                        leading: Builder(
-                                builder: (context) {if(selected==index){
-                                return Icon(Icons.check_circle);
-                              }else{
-                                return Icon(Icons.check_circle_outline);
-                              }}),
-                        title: Text(
-                              '${whoDoes[index]}'),
-                        subtitle: Text('${subWho[index]}'),
-                        onTap: (){
-                          selected=index;
-                          setState(() {
-                            
-                          });
-                        } ,
-                      );
-                    },
-                  )
-                ],)
-            ),]
-        
-      );
-  }
-}
 
-class SetMoney extends StatelessWidget {
+  Widget _choosePerson() {
+    return ListView(
+          children: <Widget>[
+            Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5),
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, 
+                    children: <Widget>[
+                      Text('Who do you want to do it',style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),),
+                      ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(vertical: 5.0),
+                        shrinkWrap: true,
+                        itemCount: whoDoes.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            dense: true,
+                            leading: Builder(
+                                    builder: (context) {if(selected==index){
+                                    return Icon(Icons.check_circle);
+                                  }else{
+                                    return Icon(Icons.check_circle_outline);
+                                  }}),
+                            title: Text(
+                                  '${whoDoes[index]}'),
+                            subtitle: Text('${subWho[index]}'),
+                            onTap: (){
+                              selected=index;
+                              setState(() {
+                                
+                              });
+                            } ,
+                          );
+                        },
+                      )
+                    ],)
+                ),]
+            
+          );
+  }
+  
+  // _setMoney state:
   final moneyController = MoneyMaskedTextController(decimalSeparator: '.', thousandSeparator: ',');
-  @override
-  Widget build(BuildContext context) {
+
+  Widget _setMoney() {
     return ListView(
       children: <Widget>[
         Container(
@@ -230,18 +273,12 @@ class SetMoney extends StatelessWidget {
         
       );
   }
-}
 
-class ChooseCharity extends StatefulWidget {
-  @override
-  _ChooseCharityState createState() => _ChooseCharityState();
-}
-
-class _ChooseCharityState extends State<ChooseCharity> {
+  // _chooseCharity state:
   final List<String> charities = <String>["Cancer Research",'British Heart Foundation','Oxfam'];
   int charity = -1;
-  @override
-  Widget build(BuildContext context) {
+  
+  Widget _chooseCharity() {
     return ListView(
       children: <Widget>[
         Container(
@@ -283,46 +320,13 @@ class _ChooseCharityState extends State<ChooseCharity> {
                   )
                 ],)
             ),]
-        
       );
   }
-}
 
-class ImageUpload extends StatefulWidget {
-  @override
-  _ImageUploadState createState() => _ImageUploadState();
-}
-
-class _ImageUploadState extends State<ImageUpload> {
-
+  // _imageUpload state
   PickedFile imageFile;
   final picker = ImagePicker();
-
-  _openGallery() async {
-    imageFile = await picker.getImage(source: ImageSource.gallery);
-    this.setState(() { });
-  }
-
-  _openCamera() async {
-    imageFile = await picker.getImage(source: ImageSource.camera);
-    this.setState(() { });
-  }
-
-  _removePhoto() {
-    imageFile = null;
-    this.setState(() { });
-  }
-
-  Widget _decideImageView() {
-    if (imageFile == null) {
-      return Center( child: Text('No image selected'));
-    } else {
-      return Image.file(File(imageFile.path));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _imageUpload() {
     return ListView(
       children: <Widget>[
         Container(
@@ -364,68 +368,86 @@ class _ImageUploadState extends State<ImageUpload> {
     );
   }
 
-
-void _changePic() {
-  showModalBottomSheet(
-    context: context, 
-    builder: (context) {
-      return Container(
-      color: Color(0xFF737373),
-      height: 350,
-      child: Container(
-        child: _buildBottomNavigationMenu(context),
-        decoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-          borderRadius: BorderRadius.only(
-            topLeft: const Radius.circular(10),
-            topRight: const Radius.circular(10),
-            ),
+  // Helper functions for the image picker
+  _openGallery() async {
+    imageFile = await picker.getImage(source: ImageSource.gallery);
+    this.setState(() { });
+  }
+  _openCamera() async {
+    imageFile = await picker.getImage(source: ImageSource.camera);
+    this.setState(() { });
+  }
+  _removePhoto() {
+    imageFile = null;
+    this.setState(() { });
+  }
+  Widget _decideImageView() {
+    if (imageFile == null) {
+      return Center( child: Text('No image selected'));
+    } else {
+      return Image.file(File(imageFile.path));
+    }
+  }
+  void _changePic() {
+    showModalBottomSheet(
+      context: context, 
+      builder: (context) {
+        return Container(
+        color: Color(0xFF737373),
+        height: 350,
+        child: Container(
+          child: _buildBottomNavigationMenu(context),
+          decoration: BoxDecoration(
+            color: Theme.of(context).canvasColor,
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(10),
+              topRight: const Radius.circular(10),
+              ),
+          ),
         ),
-      ),
-    );
+      );
     }
   );
 }
-
-ListView _buildBottomNavigationMenu(context) {
-return ListView(
-  children: <Widget>[
-    ListTile(
-      leading: Icon(FontAwesome.trash_o),
-      title: Text('Remove Current Photo'),
-      onTap: () async {
-        _removePhoto();
-        },
-    ),
-    ListTile(
-      leading: Icon(FontAwesome5Brands.facebook_square),
-      title: Text('Import from Facebook'),
-      onTap: () {
-        },
-    ),
-            ListTile(
-      leading: Icon(FontAwesome.camera),
-      title: Text('Take Photo'),
-      onTap: () {
-        _openCamera();
-        },
-    ),
-            ListTile(
-      leading: Icon(FontAwesome.image),
-      title: Text('Choose From Library'),
-      onTap: () {
-        _openGallery();
-        },
-    ),
-  ],
-  );
+  ListView _buildBottomNavigationMenu(context) {
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(FontAwesome.trash_o),
+        title: Text('Remove Current Photo'),
+        onTap: () async {
+          _removePhoto();
+          },
+      ),
+      ListTile(
+        leading: Icon(FontAwesome5Brands.facebook_square),
+        title: Text('Import from Facebook'),
+        onTap: () {
+          },
+      ),
+              ListTile(
+        leading: Icon(FontAwesome.camera),
+        title: Text('Take Photo'),
+        onTap: () {
+          _openCamera();
+          },
+      ),
+              ListTile(
+        leading: Icon(FontAwesome.image),
+        title: Text('Choose From Library'),
+        onTap: () {
+          _openGallery();
+          },
+      ),
+    ],
+    );
   }
 
 }
 
-Route _viewPost() {
+Route _viewPost(Post postData) {
   return PageRouteBuilder(
-    pageBuilder: (c, a1, a2) => ViewPost(),
+    pageBuilder: (c, a1, a2) => ViewPost(postData: postData),
     transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
     transitionDuration: Duration(milliseconds: 300),
   );
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fundder/models/post.dart';
 import 'package:fundder/models/user.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DatabaseService {
 
@@ -27,11 +31,9 @@ class DatabaseService {
     return userCollection.snapshots();
   }
 
-  // Get posts list stream is mapped to the Post object
-  List<Post> _postsDataFromSnapshot(QuerySnapshot snapshot) {
-    return snapshot.documents.map((doc){
-      // print(doc.data['timestamp'].toString());
-      return Post(
+  // Given a document return a Post type
+  Post _makePost(DocumentSnapshot doc){
+    return Post(
         author: doc.data['author'],
         title: doc.data['title'],
         charity: doc.data['charity'],
@@ -41,13 +43,80 @@ class DatabaseService {
         comments: doc.data['comments'],
         subtitle: doc.data['subtitle'],
         timestamp: doc.data['timestamp'],
+        imageUrl: doc.data['imageUrl'],
       );
+  }
+
+  // Get posts list stream is mapped to the Post object
+  List<Post> _postsDataFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((DocumentSnapshot doc){
+      // print(doc.data['timestamp'].toString());
+      return _makePost(doc);
     }).toList();
   }
+
   // Get list of posts
   Stream<List<Post>> get posts {
-    return postsCollection.snapshots().map(_postsDataFromSnapshot);
+    return postsCollection.orderBy("timestamp", descending: true).limit(10).snapshots().map(_postsDataFromSnapshot);
   }
-  
+
+  // Upload post and return the document id
+  Future uploadPost(Post post) async {
+    return await postsCollection.add({
+        "author": post.author,
+        "title": post.title,
+        "charity": post.charity,
+        "amountRaised": post.amountRaised,
+        "targetAmount": post.targetAmount,
+        "likes": post.likes,
+        "comments": post.comments,
+        "subtitle": post.subtitle,
+        "timestamp": post.timestamp,
+        "imageUrl": post.imageUrl,
+    })
+    .then((DocumentReference docRef) => {
+      docRef.documentID.toString()
+    })
+    .catchError((error) => {
+      print(error)
+    });
+  }
+
+  Future getPostById(String documentId) async {
+    String formattedId = documentId.substring(1, documentId.length-1);
+    DocumentReference docRef = postsCollection.document(formattedId);
+    return await docRef.get().then((DocumentSnapshot doc) {
+        print(doc);
+        if (doc.exists) {
+            print("Document data:" + doc.data.toString());
+            return _makePost(doc);
+        } else {
+            // doc.data() will be undefined in this case
+            print("Error - the post you are looking for doesn't exist.");
+            return null;
+        }
+    }).catchError((error) {
+        print("Error getting document: " + error);
+        return null;
+    });
+  }
+
+  // Storage ref: 
+  // Images are stored as <root>/images/<uid>/<milliseconds-from-epoch>
+  // BE AWARE: this convention will cause a naming conflict if one user uploads multiple images at the exact same time
+  final StorageReference storageRef = FirebaseStorage().ref().child("images/");
+
+  // Image upload
+  Future uploadImage(PickedFile file, String location) async {
+    var imageRef = storageRef.child(location);
+    var uploadTask = imageRef.putFile(File(file.path));
+    // await the task uploaded
+    var storageTaskSnapshot = await uploadTask.onComplete;
+    // then return the downloadURL
+    if(storageTaskSnapshot != null){
+      var downloadUrl = await imageRef.getDownloadURL();
+      return downloadUrl.toString();
+    }
+  }
 
 }
