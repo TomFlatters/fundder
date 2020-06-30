766bbae17f5ee3e80c235f61e290b188c07b1d0d
adrikoz <adrikozh@gmail.com>
2020-06-30 12:19:24 +0100

    fluro navigation routes all defined

--- lib/other_user_profile.dart
+++ lib/other_user_profile.dart
@@ -6,14 +6,13 @@ import 'helper_classes.dart';
 
 class ViewUser extends StatefulWidget {
   @override
- _ViewUserState createState() => _ViewUserState();
+  _ViewUserState createState() => _ViewUserState();
   ViewUser();
 }
 
-class _ViewUserState extends State<ViewUser> with SingleTickerProviderStateMixin {
-
-
-TabController _tabController;
+class _ViewUserState extends State<ViewUser>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
 
   @override
   void dispose() {
@@ -34,97 +33,107 @@ TabController _tabController;
     }
   }
 
-
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Name'),
         actions: <Widget>[
           new IconButton(
-            icon: new Icon(Icons.close), 
+            icon: new Icon(Icons.close),
             onPressed: () => Navigator.of(context).pop(null),
-            )
+          )
         ],
         leading: new Container(),
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 20, bottom:10),
-            alignment: Alignment.center,
-            child: Container(
-              child: ProfilePic("https://i.imgur.com/BoN9kdC.png", 90),
-              margin: EdgeInsets.all(10.0),            
-            ),
-          ), Center(
-            child: Text("@username"),
-          ), Container(
-            margin: EdgeInsets.symmetric(horizontal:50, vertical: 20),
+      body: ListView(shrinkWrap: true, children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 20, bottom: 10),
+          alignment: Alignment.center,
+          child: Container(
+            child: ProfilePic("https://i.imgur.com/BoN9kdC.png", 90),
+            margin: EdgeInsets.all(10.0),
+          ),
+        ),
+        Center(
+          child: Text("@username"),
+        ),
+        Container(
+            margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
             height: 50,
             child: Row(
               children: <Widget>[
-                Expanded( child:GestureDetector(
-                  child: Column( children: <Widget>[
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Text("54", 
-                      style: TextStyle(
-                      fontSize: 20, 
-                      fontWeight: FontWeight.bold,
-                      )
-                    ),
+                Expanded(
+                    child: GestureDetector(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.topCenter,
+                        child: Text("54",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.bottomCenter,
+                        child: Text("Following"),
+                      )),
+                    ],
                   ),
-                  Expanded( child:Container(
-                    alignment: Alignment.bottomCenter,
-                    child: Text("Following"),
-                  )),
-                  ],),
-                  onTap: () {Navigator.of(context).push(_viewFollowers());},
-                )),
-                Expanded( child:GestureDetector(
-                  child: Column( children: <Widget>[
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Text("106", 
-                      style: TextStyle(
-                      fontSize: 20, 
-                      fontWeight: FontWeight.bold,
-                      )
-                    ),
-                  ),
-                  Expanded( child:Container(
-                    alignment: Alignment.bottomCenter,
-                    child: Text("Followers"),
-                  )),
-                  ],),
-                  onTap: () {Navigator.of(context).push(_viewFollowers());},
+                  onTap: () {
+                    Navigator.pushNamed(context, '/username/followers');
+                  },
                 )),
                 Expanded(
-                  child: Column( children: <Widget>[
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child: Text("£54", 
-                      style: TextStyle(
-                      fontSize: 20, 
-                      fontWeight: FontWeight.bold,
-                      )
-                    ),
+                    child: GestureDetector(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.topCenter,
+                        child: Text("106",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Expanded(
+                          child: Container(
+                        alignment: Alignment.bottomCenter,
+                        child: Text("Followers"),
+                      )),
+                    ],
                   ),
-                  Expanded( child:Container(
-                    alignment: Alignment.bottomCenter,
-                    child: Text("Raised"),
-                  )),
-                  ],)
-                ),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/username/followers');
+                  },
+                )),
+                Expanded(
+                    child: Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Text("£54",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                    Expanded(
+                        child: Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Text("Raised"),
+                    )),
+                  ],
+                )),
               ],
-            )
-          ),  GestureDetector(
+            )),
+        GestureDetector(
             child: Container(
               padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-              margin: EdgeInsets.only(left: 70, right:70, bottom: 20),
+              margin: EdgeInsets.only(left: 70, right: 70, bottom: 20),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey, width: 1),
                 borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -133,47 +142,30 @@ Widget build(BuildContext context) {
                 "Follow",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.black,
                 ),
               ),
-            onTap: (){
-            }
-          ), DefaultTabController(
-            length: 2,
-            initialIndex: 0,
-            child: Column(
-              children: [
-                TabBar(
-                  tabs: [Tab(text: 'Posts'), Tab(text: 'Liked')],
-                  controller: _tabController,
-                  ),
-                  [FeedView('user', 'adrikoz', Colors.black),
-                  FeedView('user', 'adrikoz', Colors.blue),][_tabController.index]
-                /*ConstrainedBox(
-                  constraints: BoxConstraints(maxHeight: 1000),
-                  child: TabBarView(
-                      children: [
-                        FeedView('user', Colors.black),
-                        FeedView('user', Colors.black),
-                        ],
-                    )
-                )*/
-              ],
-            ), 
-          )
-        ]
-      ),
+            ),
+            onTap: () {}),
+        DefaultTabController(
+          length: 2,
+          initialIndex: 0,
+          child: Column(
+            children: [
+              TabBar(
+                tabs: [Tab(text: 'Posts'), Tab(text: 'Liked')],
+                controller: _tabController,
+              ),
+              [
+                FeedView('user', 'adrikoz', Colors.black),
+                FeedView('user', 'adrikoz', Colors.blue),
+              ][_tabController.index]
+            ],
+          ),
+        )
+      ]),
     );
- }
-}
-
-Route _viewFollowers() {
-  return PageRouteBuilder(
-    pageBuilder: (c, a1, a2) => ViewFollowers(),
-    transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
-    transitionDuration: Duration(milliseconds: 300),
-  );
+  }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,18 +2,17 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:fundder/other_user_profile.dart';
 import 'package:fundder/view_post_controller.dart';
-import 'package:fundder/web_pages/test_controller.dart';
 import 'package:fundder/comment_view_controller.dart';
 import 'package:fundder/donate_page_controller.dart';
 import 'package:fundder/add_post_controller.dart';
+import 'package:fundder/do_challenge_detail.dart';
+import 'package:fundder/challenge_steps_view.dart';
+import 'package:fundder/view_followers_controller.dart';
 import 'package:fundder/profile_controller.dart';
 import 'package:fundder/edit_profile_controller.dart';
 
 class FluroRouter {
   static Router router = Router();
-  static Handler _testHandler = Handler(
-      handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
-          TestPost(postData: params['id'][0]));
   static Handler _postHandler = Handler(
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
           ViewPost(postData: params['id'][0]));
@@ -26,19 +25,23 @@ class FluroRouter {
   static Handler _viewOtherProfileHandler = Handler(
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
           ViewUser());
+  static Handler _followersHandler = Handler(
+      handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
+          ViewFollowers());
   static Handler _editProfileHandler = Handler(
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
-          ViewPost(postData: params['id'][0]));
+          EditProfile());
   static Handler _addPostHandler = Handler(
       handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
           AddPost());
+  static Handler _challengeDetailHandler = Handler(
+      handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
+          ChallengeDetail(challengeId: params['id'][0]));
+  static Handler _challengeStepsHandler = Handler(
+      handlerFunc: (BuildContext context, Map<String, dynamic> params) =>
+          StepsPage(challengeId: params['id'][0]));
 
   static void setupRouter() {
-    router.define(
-      '/share/:id',
-      handler: _testHandler,
-      transitionType: TransitionType.fadeIn,
-    );
     router.define(
       '/post/:id',
       handler: _postHandler,
@@ -57,12 +60,24 @@ class FluroRouter {
       handler: _viewOtherProfileHandler,
     );
     router.define(
+      '/username/followers',
+      handler: _followersHandler,
+    );
+    router.define(
       '/account/edit',
       handler: _editProfileHandler,
     );
     router.define(
       '/addpost',
       handler: _addPostHandler,
+    );
+    router.define(
+      '/challenge/:id',
+      handler: _challengeDetailHandler,
+    );
+    router.define(
+      '/challenge/:id/steps',
+      handler: _challengeStepsHandler,
     );
   }
 }
