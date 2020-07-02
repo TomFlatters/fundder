ecddb1ff6edd499236031214f6d40b01f87fc4c8
Tom Flatters <tflats.tf@gmail.com>
2020-07-02 13:59:00 +0100

    Setup template.dart

--- lib/models/template.dart
+++ lib/models/template.dart
@@ -1,5 +1,19 @@
 import 'package:fundder/models/post.dart';
 
 class Template extends Post {
-    
+    String whoDoes;
+
+    Template(
+      {author,
+      title,
+      charity,
+      amountRaised,
+      targetAmount,
+      subtitle,
+      likes,
+      comments,
+      timestamp,
+      imageUrl,
+      id,
+      this.whoDoes});
 }
