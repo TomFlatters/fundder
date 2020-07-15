91f2244978470d816b3c5d058147e3e8f5f5eb98
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-07-15 17:26:07 +0100

    payment ui v1 done

--- lib/donation/donate_page_controller.dart
+++ lib/donation/donate_page_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:fundder/donation/money_input_text_widget.dart';
+import 'package:fundder/donation/payment_methods.dart';
 import 'package:fundder/main.dart';
 import '../helper_classes.dart';
 
@@ -18,19 +19,50 @@ class _DonatePageState extends State<DonatePage> {
 
   @override
   Widget build(BuildContext context) {
+    final moneyController = MoneyMaskedTextController(
+        decimalSeparator: '.', thousandSeparator: ',');
+    ThemeData theme = Theme.of(context);
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Donate"),
-          actions: <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.close),
-              onPressed: () => Navigator.of(context).pop(null),
-            )
-          ],
-        ),
-        body: Column(
-          children: <Widget>[moneyInput],
-        ));
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Donate"),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.close),
+            onPressed: () => Navigator.of(context).pop(null),
+          )
+        ],
+        leading: new Container(),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 5),
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Thank you for your genorisity. \nPlease enter your donation amount.',
+                      style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    moneyInputWidget(moneyController)
+                  ])),
+          ListTile(
+            leading: Icon(Icons.add_circle, color: Colors.black),
+            title: Text("Pay via new card"),
+          ),
+          ListTile(
+            leading: Icon(Icons.credit_card, color: Colors.black),
+            title: Text("Pay via existing cards"),
+          )
+        ],
+      ),
+    );
   }
 }
