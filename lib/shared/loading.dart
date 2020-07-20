67b7d764e75408d5e594ef6a35c8cfa4b8440f12
adrikoz <adrikozh@gmail.com>
2020-07-20 15:39:38 +0100

    all videos done

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -12,35 +12,34 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> with TickerProviderStateMixin {
-
   Animation<double> animation;
   AnimationController _controller;
   int phase = 1;
-  List sizeList = [50.0,75.0];
+  List sizeList = [50.0, 75.0];
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: const Duration(milliseconds: 750), vsync: this);
-    animation = Tween<double>(begin: 50, end: 100).animate(_controller) 
-    ..addListener(() {  
-      setState(() {          
-        /*if (status == AnimationStatus.completed) {            
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 750), vsync: this);
+    animation = Tween<double>(begin: 50, end: 100).animate(_controller)
+      ..addListener(() {
+        setState(() {
+          /*if (status == AnimationStatus.completed) {            
           _controller.reverse();            
         } else if (status == AnimationStatus.dismissed) {            
           _controller.forward();            
         }*/
-      });            
-    })
-    ..addStatusListener((status) {
-      if (status == AnimationStatus.completed) {            
-          _controller.reverse();            
-        } else if (status == AnimationStatus.dismissed) {            
-          _controller.forward();            
+        });
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _controller.forward();
         }
-    });        
+      });
     _controller.forward();
-
   }
 
   @override
@@ -49,13 +48,18 @@ class _LoadingState extends State<Loading> with TickerProviderStateMixin {
       color: Colors.white,
       child: Center(
         child: Container(
-            // Use the properties stored in the State class.
-            width: animation.value,
-            height: animation.value,
-            child: Image.asset('assets/images/fundder_loading.png'),
-            // Define how long the animation should take.
-            // Provide an optional curve to make the animation feel smoother.
-          ),/*SpinKitChasingDots(
+            height: MediaQuery.of(context).size.width,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Container(
+                // Use the properties stored in the State class.
+                width: animation.value,
+                height: animation.value,
+                child: Image.asset('assets/images/fundder_loading.png'),
+              ),
+              // Define how long the animation should take.
+              // Provide an optional curve to make the animation feel smoother.
+            )), /*SpinKitChasingDots(
                 color: Color(0xffA3D165),
                 size: 50.0,
             ),*/
@@ -63,9 +67,9 @@ class _LoadingState extends State<Loading> with TickerProviderStateMixin {
     );
   }
 
-  @override            
-  void dispose() {            
-    _controller.dispose();            
-    super.dispose();            
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
