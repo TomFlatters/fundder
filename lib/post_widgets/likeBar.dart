9bf6744e7de39b711e90f13e9a1e712fb51734dd
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-07-23 13:48:12 +0100

    a stateful likebar added

---
+++ lib/post_widgets/likeBar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import '../models/user.dart';
+import '../services/likes.dart';
+
+class LikeBar extends StatefulWidget {
+  final String postId;
+  LikeBar({this.postId});
+  @override
+  _LikeBarState createState() => _LikeBarState(postId: postId);
+}
+
+class _LikeBarState extends State<LikeBar> {
+  final String postId;
+  _LikeBarState({this.postId});
+
+  FlatButton createHeart(bool hasLiked, Function callBack) {
+    return FlatButton(
+        child: Container(
+            width: 20,
+            height: 20,
+            padding: const EdgeInsets.all(0.0),
+            child: (hasLiked)
+                ? Image.asset('assets/images/like_selected.png')
+                : Image.asset('assets/images/like.png')),
+        onPressed: callBack);
+  }
+
+  void _likePost() {
+    setState() {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    final LikesService likesService = LikesService(uid: user.uid);
+
+    return Row(
+      children: <Widget>[
+        FutureBuilder(
+            future: likesService.hasUserLikedPost(postId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return createHeart(snapshot.data, _likePost);
+              } else {
+                return Container();
+              }
+            }),
+        FutureBuilder(
+            future: likesService.noOfLikes(postId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(snapshot.toString()));
+              } else {
+                return Container();
+              }
+            })
+      ],
+    );
+  }
+}
