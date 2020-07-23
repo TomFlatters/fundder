1d5c4d098135b70f696028b61ec05a8202d7caed
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-07-23 16:41:33 +0100

    UI bug fixed--heart showing

--- lib/post_widgets/likeBar.dart
+++ lib/post_widgets/likeBar.dart
@@ -18,13 +18,10 @@ class _LikeBarState extends State<LikeBar> {
 
   FlatButton createHeart(bool hasLiked, LikesService likesService) {
     return FlatButton(
-        child: Container(
-            width: 20,
-            height: 20,
-            padding: const EdgeInsets.all(0.0),
-            child: (hasLiked)
-                ? Image.asset('assets/images/like_selected.png')
-                : Image.asset('assets/images/like.png')),
+        padding: EdgeInsets.all(0.0),
+        child: (hasLiked)
+            ? Image.asset('assets/images/like_selected.png')
+            : Image.asset('assets/images/like.png'),
         onPressed: () {
           setState(() {
             print("I'm pressing like!!!!!");
@@ -44,26 +41,34 @@ class _LikeBarState extends State<LikeBar> {
 
     return Row(
       children: <Widget>[
-        FutureBuilder(
-            future: likesService.hasUserLikedPost(postId),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return createHeart(snapshot.data, likesService);
-              } else {
-                return Container();
-              }
-            }),
-        FutureBuilder(
-            future: likesService.noOfLikes(postId),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Container(
-                    margin: EdgeInsets.only(left: 10),
-                    child: Text(snapshot.data.toString()));
-              } else {
-                return Container();
-              }
-            })
+        Container(
+          height: 20,
+          width: 20,
+          child: FutureBuilder(
+              future: likesService.hasUserLikedPost(postId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return createHeart(snapshot.data, likesService);
+                } else {
+                  return Container();
+                }
+              }),
+        ),
+        Expanded(
+            child: FutureBuilder(
+                future: likesService.noOfLikes(postId),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Text(
+                          snapshot.data.toString(),
+                          textAlign: TextAlign.left,
+                        ));
+                  } else {
+                    return Container();
+                  }
+                }))
       ],
     );
   }
