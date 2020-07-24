e4f3cda4d4aa1e05c8c3ec5b2d32b406ea1e4bb2
adrikoz <adrikozh@gmail.com>
2020-07-24 17:31:00 +0100

    notifications sent on like

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,77 @@ const functions = require('firebase-functions');
 //   response.send("Hello from Firebase!");
 // });
 
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    functions.logger.log("req:", req);
+    functions.logger.log("req body:", req['body']);
+    functions.logger.log("req.query.text:", req.query.text);
+    // Grab the text parameter.
+    const original = req['body']['data']['text'];
+    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+
+exports.onLike = functions.firestore
+    .document('/posts/{postId}/whoLiked/{userId}')
+    .onCreate(async (snapshot, context) =>
+    {
+    const userId = context.params.userId;
+    const userRef = admin.firestore().doc(`users/${userId}`);
+    const docLiker = await userRef.get();
+    const payload =
+    {
+        notification: { 
+            title: 'Like',
+            body: `${docLiker.username} liked your post`, 
+        },
+    };
+
+    const postId = context.params.postId;
+    const doc = await admin.firestore().doc(`posts/${postId}`).get();
+
+    console.log("doc " + doc.data());
+
+    const author = doc.data()['author'];
+
+    console.log("author " + author);
+
+    const postOwner = await admin.firestore().doc(`users/${author}`).get();
+
+    console.log("owner " + postOwner.data());
+
+    const tokens = [postOwner.data()['fcm']];
+
+    if (tokens.length > 0) {
+    // Send notifications to all tokens.
+    const response = await admin.messaging().sendToDevice(tokens, payload);
+    await cleanupTokens(response, tokens);
+    console.log('Notifications have been sent and tokens cleaned up.');
+    }
+});
+
+// Cleans up the tokens that are no longer valid.
+function cleanupTokens(response, tokens) {
+    // For each notification we check if there was an error.
+    const tokensDelete = [];
+    response.results.forEach((result, index) => {
+      const error = result.error;
+      if (error) {
+        console.error('Failure sending notification to', tokens[index], error);
+        // Cleanup the tokens who are not registered anymore.
+        if (error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered') {
+          const deleteTask = admin.firestore().collection('fcmTokens').doc(tokens[index]).delete();
+          tokensDelete.push(deleteTask);
+        }
+      }
+    });
+    return Promise.all(tokensDelete);
+   }
