785e273ab10c4a2e1d4b7d28ad5574e5333a8f6a
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-07-26 16:51:51 +0100

    comment stream for post postId added

--- lib/services/comments_service.dart
+++ lib/services/comments_service.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CommentsService {
   final String uid;
-  CommentsService({this.uid});
+  final String postId;
+  CommentsService({this.uid, this.postId});
 
   final CollectionReference postsCollection =
       Firestore.instance.collection('posts');
@@ -10,8 +11,18 @@ class CommentsService {
   final CollectionReference userCollection =
       Firestore.instance.collection('users');
 
+  Stream<List<DocumentSnapshot>> get comments {
+    print('returning comments for  post: {$postId}');
+    return postsCollection
+        .document(postId)
+        .collection('comments')
+        .orderBy("timestamp", descending: true)
+        .snapshots()
+        .map((q) => q.documents);
+  }
+
   //add a comment to a post postId
-  void addAcomment(String postId, Map comment) {
+  void addAcomment(Map comment) {
     postsCollection.document(postId).collection('comments').add({
       'uid': uid,
       'username': comment['username'],
