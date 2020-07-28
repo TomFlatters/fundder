ccff6cae6eef48953e4b9df18aa477394da470ab
adrikoz <adrikozh@gmail.com>
2020-07-28 12:05:40 +0100

    layout of search_descriptor changed

--- lib/search/search_descriptor.dart
+++ lib/search/search_descriptor.dart
@@ -4,29 +4,25 @@ import 'package:flutter_icons/flutter_icons.dart';
 class SearchDescriptor extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      color: Colors.white,
-      height: MediaQuery.of(context).size.width,
-      child: Center(
-          child: Container(
-        child: Column(children: [
-          Icon(
-            AntDesign.search1,
-            color: Colors.grey[300],
-            size: MediaQuery.of(context).size.width / 2,
-          ),
-          Container(
-              margin: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-              child: Text(
-                'Search for users by username or for posts by keyword',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
-                    fontFamily: 'Roboto Mono'),
-              )),
-        ]),
-      )),
-    ));
+      child: Container(
+          margin: EdgeInsets.only(top: 100),
+          child: Column(children: [
+            Icon(
+              AntDesign.search1,
+              color: Colors.grey[300],
+              size: MediaQuery.of(context).size.width / 4,
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 30, left: 40, right: 40),
+                child: Text(
+                  'Search for users by username or for posts by keyword',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontFamily: 'Roboto Mono'),
+                )),
+          ])),
+    );
   }
 }
