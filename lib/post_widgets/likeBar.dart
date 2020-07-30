fb289cac8a4c2b8484788c7650de5b57cbdd9acb
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-07-31 00:17:12 +0100

    removed clunky named args from man...SetState

--- lib/post_widgets/likeBar.dart
+++ lib/post_widgets/likeBar.dart
@@ -38,7 +38,7 @@ class LikesModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  void manuallySetState({int noLikes, bool isLiked}) {
+  void manuallySetState(int noLikes, bool isLiked) {
     _noLikes = noLikes;
     _isLiked = isLiked;
     notifyListeners();
@@ -47,7 +47,7 @@ class LikesModel extends ChangeNotifier {
 
 class LikeBar extends StatelessWidget {
   //the widget will listen use likesModel to interact with the state. It will be rebuilt everytime the state changes.
-
+  //must be placed in below a LikesModel
   @override
   Widget build(BuildContext context) {
     var likesModel = Provider.of<LikesModel>(context);
@@ -79,74 +79,3 @@ class LikeBar extends StatelessWidget {
         ]));
   }
 }
-
-// class LikeBar extends StatefulWidget {
-//   final String postId;
-//   LikeBar({
-//     this.postId,
-//   });
-//   @override
-//   _LikeBarState createState() => _LikeBarState(postId: postId);
-// }
-
-// class _LikeBarState extends State<LikeBar> {
-//   final String postId;
-//   _LikeBarState({this.postId});
-
-//   FlatButton createHeart(bool hasLiked, LikesService likesService) {
-//     return FlatButton(
-//         padding: EdgeInsets.only(left: 10.0),
-//         child: (hasLiked)
-//             ? Image.asset('assets/images/like_selected.png')
-//             : Image.asset('assets/images/like.png'),
-//         onPressed: () {
-//           setState(() {
-//             print("I'm pressing like!!!!!");
-//             if (hasLiked) {
-//               likesService.unlikePost(postId);
-//             } else {
-//               likesService.likePost(postId);
-//             }
-//           });
-//         });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final user = Provider.of<User>(context);
-//     final LikesService likesService = LikesService(uid: user.uid);
-
-//     return Row(
-//       children: <Widget>[
-//         Container(
-//           height: 20,
-//           width: 30,
-//           child: FutureBuilder(
-//               future: likesService.hasUserLikedPost(postId),
-//               builder: (context, snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.done) {
-//                   return createHeart(snapshot.data, likesService);
-//                 } else {
-//                   return Container();
-//                 }
-//               }),
-//         ),
-//         Expanded(
-//             child: FutureBuilder(
-//                 future: likesService.noOfLikes(postId),
-//                 builder: (context, snapshot) {
-//                   if (snapshot.connectionState == ConnectionState.done) {
-//                     return Container(
-//                         margin: EdgeInsets.only(left: 10),
-//                         child: Text(
-//                           snapshot.data.toString(),
-//                           textAlign: TextAlign.left,
-//                         ));
-//                   } else {
-//                     return Container();
-//                   }
-//                 }))
-//       ],
-//     );
-//   }
-// }
