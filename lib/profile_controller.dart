88851409536c4e7236cf8a9d6802ef2e51bd5eb4
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-08-04 00:42:03 +0100

    navigation to viewpost added

--- lib/profile_controller.dart
+++ lib/profile_controller.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fundder/post_widgets/likeBar.dart';
 import 'package:fundder/services/auth.dart';
+import 'package:fundder/services/likes.dart';
 import 'package:fundder/view_post_controller.dart';
 import 'feed.dart';
 import 'profile_actions_view.dart';
@@ -308,24 +310,51 @@ class _ProfileState extends State<ProfileController>
   }
 
   Widget _profileView(String uid) {
+    LikesService likesService = LikesService(uid: uid);
+
     return ListView.builder(
         physics: NeverScrollableScrollPhysics(),
         shrinkWrap: true,
         itemCount: postList != null ? postList.length : 0,
         itemBuilder: (BuildContext context, int index) {
+          bool initiallyHasLiked;
+          int initialLikesNo;
           Post post = postList[index];
-          return ListTile(
-            leading: ProfilePic(post.author, 40),
-            title: Text(post.title),
-            subtitle: Text(post.subtitle),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ViewPost(postData: post.id, likesModel: null)));
-            },
-          );
+          return FutureBuilder(
+              future: likesService.hasUserLikedPost(post.id),
+              builder: (context, hasLiked) {
+                if (hasLiked.connectionState == ConnectionState.done) {
+                  initiallyHasLiked = hasLiked.data;
+                  return FutureBuilder(
+                    future: likesService.noOfLikes(post.id),
+                    builder: (context, noLikes) {
+                      if (noLikes.connectionState == ConnectionState.done) {
+                        initialLikesNo = noLikes.data;
+                        var likesModel = LikesModel(
+                            initiallyHasLiked, initialLikesNo,
+                            uid: uid, postId: post.id);
+                        return ListTile(
+                          leading: ProfilePic(post.author, 40),
+                          title: Text(post.title),
+                          subtitle: Text(post.subtitle),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ViewPost(
+                                        postData: post.id,
+                                        likesModel: likesModel)));
+                          },
+                        );
+                      } else {
+                        return ListTile();
+                      }
+                    },
+                  );
+                } else {
+                  return ListTile();
+                }
+              });
         });
   }
 
