31a96a9d2d5864120f093268f1a70e39cd50dab4
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-08-07 16:24:21 +0100

    posts don't flash --state moved further down

--- lib/feed.dart
+++ lib/feed.dart
@@ -63,6 +63,7 @@ class _FeedViewState extends State<FeedView> {
 
   @override
   Widget build(BuildContext context) {
+    LikesModel likesModel;
     print("feed being built");
     final user = Provider.of<User>(context);
     final LikesService likesService = LikesService(uid: user.uid);
@@ -80,115 +81,116 @@ class _FeedViewState extends State<FeedView> {
           // print(postData)
           bool initiallyHasLiked;
           int initialLikesNo;
-          return FutureBuilder(
-              future: likesService.hasUserLikedPost(postData.id),
-              builder: (context, hasLiked) {
-                if (hasLiked.connectionState == ConnectionState.done) {
-                  initiallyHasLiked = hasLiked.data;
-                  return FutureBuilder(
-                    future: likesService.noOfLikes(postData.id),
-                    builder: (context, noLikes) {
-                      if (noLikes.connectionState == ConnectionState.done) {
-                        initialLikesNo = noLikes.data;
-                        var likesModel = LikesModel(
-                            initiallyHasLiked, initialLikesNo,
-                            uid: user.uid, postId: postData.id);
-
-                        return GestureDetector(
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            color: Colors.white,
-                            child: Container(
-                              margin:
-                                  EdgeInsets.only(left: 0, right: 0, top: 0),
-                              child: Column(children: <Widget>[
-                                PostHeader(
-                                    postAuthorId: postData.author,
-                                    postAuthorUserName: postData.authorUsername,
-                                    targetCharity: postData.charity,
-                                    postStatus: postData.status),
-                                PostBody(postData: postData),
-                                Container(
-                                  //action bar
-                                  key: GlobalKey(),
-                                  height: 30,
-                                  child: Row(children: <Widget>[
-                                    Expanded(
+          return GestureDetector(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Container(
+                margin: EdgeInsets.only(left: 0, right: 0, top: 0),
+                child: Column(children: <Widget>[
+                  PostHeader(
+                      postAuthorId: postData.author,
+                      postAuthorUserName: postData.authorUsername,
+                      targetCharity: postData.charity,
+                      postStatus: postData.status),
+                  PostBody(postData: postData),
+                  Container(
+                    //action bar
+                    key: GlobalKey(),
+                    height: 30,
+                    child: Row(children: <Widget>[
+                      FutureBuilder(
+                          future: likesService.hasUserLikedPost(postData.id),
+                          builder: (context, hasLiked) {
+                            if (hasLiked.connectionState ==
+                                ConnectionState.done) {
+                              initiallyHasLiked = hasLiked.data;
+                              return FutureBuilder(
+                                future: likesService.noOfLikes(postData.id),
+                                builder: (context, noLikes) {
+                                  if (noLikes.connectionState ==
+                                      ConnectionState.done) {
+                                    initialLikesNo = noLikes.data;
+                                    likesModel = LikesModel(
+                                        initiallyHasLiked, initialLikesNo,
+                                        uid: user.uid, postId: postData.id);
+                                    return Expanded(
                                       //like bar
                                       child: ChangeNotifierProvider(
                                           create: (context) => likesModel,
                                           child: LikeBar()),
-                                    ),
-                                    Expanded(
-                                        child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: CommentButton(
-                                        pid: postData.id,
-                                      ),
-                                    )),
-                                    Expanded(
-                                      child: ShareBar(),
-                                    ),
-                                  ]),
-                                ),
-                                Row(children: [
-                                  Expanded(
-                                    child: Container(
-                                      alignment: Alignment.centerLeft,
-                                      margin: EdgeInsets.all(10),
-                                      child: Text(
-                                        howLongAgo(postData.timestamp),
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  postData.author != user.uid
-                                      ? Container()
-                                      : FlatButton(
-                                          onPressed: () {
-                                            print('button pressed');
-                                            _showDeleteDialog(postData);
-                                          },
-                                          child: Text('Delete',
-                                              textAlign: TextAlign.right,
-                                              style: TextStyle(
-                                                fontSize: 14,
-                                                color: Colors.grey,
-                                              )),
-                                        ),
-                                ])
-                              ]),
-                            ),
+                                    );
+                                  } else {
+                                    return Container();
+                                  }
+                                },
+                              );
+                            } else {
+                              return Container();
+                            }
+                          }),
+                      Expanded(
+                          child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: CommentButton(
+                          pid: postData.id,
+                        ),
+                      )),
+                      Expanded(
+                        child: ShareBar(),
+                      ),
+                    ]),
+                  ),
+                  Row(children: [
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.all(10),
+                        child: Text(
+                          howLongAgo(postData.timestamp),
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
                           ),
-                          onTap: () async {
-                            // var pid = postData.id;
-                            // var noLikes = likesModel.noLikes;
-                            // var isLiked = likesModel.isLiked;
-                            // var uid = user.uid;
-                            // //passing state into ViewPost screen
-                            // LikesModel likeState = LikesModel(isLiked, noLikes,
-                            //     uid: uid, postId: pid);
-
-                            var newState = await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ViewPost(postData)));
-                            likesModel.manuallySetState(
-                                newState['noLikes'], newState['isLiked']);
-                          },
-                        );
-                      } else {
-                        return Container();
-                      }
-                    },
-                  );
-                } else {
-                  return Container();
-                }
-              });
+                        ),
+                      ),
+                    ),
+                    postData.author != user.uid
+                        ? Container()
+                        : FlatButton(
+                            onPressed: () {
+                              print('button pressed');
+                              _showDeleteDialog(postData);
+                            },
+                            child: Text('Delete',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                )),
+                          ),
+                  ])
+                ]),
+              ),
+            ),
+            onTap: () async {
+              // var pid = postData.id;
+              // var noLikes = likesModel.noLikes;
+              // var isLiked = likesModel.isLiked;
+              // var uid = user.uid;
+              // //passing state into ViewPost screen
+              // LikesModel likeState = LikesModel(isLiked, noLikes,
+              //     uid: uid, postId: pid);
+              if (likesModel != null) {
+                var newState = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ViewPost(postData)));
+                likesModel.manuallySetState(
+                    newState['noLikes'], newState['isLiked']);
+              }
+            },
+          );
         },
         separatorBuilder: (BuildContext context, int index) {
           return SizedBox(
