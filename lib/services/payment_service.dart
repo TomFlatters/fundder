7af40187bbf98c156d1afa5804516217fc76709c
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-08-08 16:08:27 +0100

    steps for payment business logic commented  in

--- lib/services/payment_service.dart
+++ lib/services/payment_service.dart
@@ -3,6 +3,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:stripe_payment/stripe_payment.dart';
+//dcoumentation for stripe api....flutter stripe plugin is adapted from the one
+//of react native. Same documentation applies.
+// https://tipsi.github.io/tipsi-stripe/docs/index.html
+
+/**Steps involved in a Stripe card payment: 
+ * 
+ * 1. (Server Side)Initialise Stripe with secret key in cloud functions.
+ * 
+ * 2. (Server Side) Create a function that is capable of creating a PaymentIntent.
+ *    Doing this server side removes the need to store the secret key client 
+ *    side.
+ * 
+ * 3 .(Client Side) Add an instance of Stripe’s CardInputWidget to 
+ *    your checkout form. This is used to collect card details and ensures that the
+ *    sensitive details never touch your server. You can extract a token from 
+ *    this which will serve as the payment method, instead of storing the card
+ *    details in your own servers. 
+ * 
+ *  4. (Client Side) Make a request to your server for a PaymentIntent as soon as the view
+ *    loads. Store a reference to the client secret, instead of the server 
+ *    passing back the entire. 
+ *  
+ * 5. (Client Side) Confirm payment using the PaymentIntent client secret you 
+ *    collected from your server as well as the token for the payment method.
+ * 
+ * */
 
 class StripeTransactionResponse {
   String message;
@@ -32,11 +58,21 @@ class StripeService {
       {String amount, String currency}) async {
     try {
       print('card form created and details about to be entered...');
+      //token from this payment method to be passed server side
+
       var paymentMethod = await StripePayment.paymentRequestWithCardForm(
           CardFormPaymentRequest());
-      print('payment intent being created...');
+      print('payment intent being created in backend...');
+
+      //var https callable function to start the payment...taking in the
+      //token for the payment method and the amount to be paid
+      //currency will be gbp for now
       var paymentIntent =
           await StripeService.createPaymentIntent(amount, currency);
+
+      //get client secret from paymentIntent object created server side
+
+      //confirmation of payment will be done client side
       var response = await StripePayment.confirmPaymentIntent(PaymentIntent(
           clientSecret: paymentIntent['client_secret'],
           paymentMethodId: paymentMethod.id));
