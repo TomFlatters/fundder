99871e48e3dd84132c43873f48bf8755d4f0f7b2
adrikoz <adrikozh@gmail.com>
2020-08-08 14:12:26 +0100

    added sream builder underneath stream provider to stop login screen flashing at start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fundder/services/auth.dart';
+import 'package:fundder/shared/loading.dart';
 import 'package:fundder/wrapper.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -20,33 +21,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
-      // listen to the stream specified by value
-      value: AuthService().user,
-      child: MaterialApp(
-          initialRoute: '/',
-          onGenerateRoute: FluroRouter.router.generator,
-          title: 'Fundder',
-          navigatorObservers: [routeObserver],
-          home: Wrapper(),
-          theme: ThemeData(
-              scaffoldBackgroundColor: Colors.white,
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              primaryColor: Colors.white,
-              accentColor: HexColor('b8b8d1'),
-              primaryTextTheme: TextTheme(
-                  headline6: TextStyle(
-                      //fontWeight: FontWeight.w600,
-                      fontSize: 20,
-                      fontFamily: 'Roboto Mono')),
-              tabBarTheme: TabBarTheme(
-                  labelStyle: TextStyle(
-                      fontFamily: 'Quicksand', fontWeight: FontWeight.bold),
-                  unselectedLabelStyle: TextStyle(fontFamily: 'Quicksand'),
-                  indicator: UnderlineTabIndicator(
-                      borderSide: BorderSide(width: 2.0, color: Colors.grey))),
-              fontFamily: 'Muli',
-              appBarTheme: AppBarTheme(elevation: 0))),
-    );
+        // listen to the stream specified by value
+        value: AuthService().user,
+        child: StreamBuilder<User>(
+            stream: AuthService().user,
+            builder: (context, snapshot) {
+              return MaterialApp(
+                  initialRoute: '/',
+                  onGenerateRoute: FluroRouter.router.generator,
+                  title: 'Fundder',
+                  navigatorObservers: [routeObserver],
+                  home: snapshot.connectionState != ConnectionState.waiting
+                      ? Wrapper()
+                      : Loading(),
+                  theme: ThemeData(
+                      scaffoldBackgroundColor: Colors.white,
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      primaryColor: Colors.white,
+                      accentColor: HexColor('b8b8d1'),
+                      primaryTextTheme: TextTheme(
+                          headline6: TextStyle(
+                              //fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              fontFamily: 'Roboto Mono')),
+                      tabBarTheme: TabBarTheme(
+                          labelStyle: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold),
+                          unselectedLabelStyle:
+                              TextStyle(fontFamily: 'Quicksand'),
+                          indicator: UnderlineTabIndicator(
+                              borderSide:
+                                  BorderSide(width: 2.0, color: Colors.grey))),
+                      fontFamily: 'Muli',
+                      appBarTheme: AppBarTheme(elevation: 0)));
+            }));
   }
 }
