ca06f76b443faf76f8fb7bca30a0ca48dcdace5a
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-08-22 13:15:45 +0100

    likes are now synced cleanly with feed&ViewPost

--- lib/post_widgets/likeBar.dart
+++ lib/post_widgets/likeBar.dart
@@ -8,84 +8,84 @@ import '../models/user.dart';
 import '../services/likes.dart';
 import 'package:fundder/helper_classes.dart';
 
-class LikesModel extends ChangeNotifier {
-  //the 'state' of the likes on a 'postId' post in 'uid' user's feed is housed here
-//i.e the number of likes of this post as well as well as whether uid has liked postId
-  bool _isLiked;
-  int _noLikes;
-  final String postId;
-  final String uid;
-  final LikesService _likesService;
+// class LikesModel extends ChangeNotifier {
+//   //the 'state' of the likes on a 'postId' post in 'uid' user's feed is housed here
+// //i.e the number of likes of this post as well as well as whether uid has liked postId
+//   bool _isLiked;
+//   int _noLikes;
+//   final String postId;
+//   final String uid;
+//   final LikesService _likesService;
 
-  LikesModel(bool initialIsLiked, int initialNoLikes, {this.uid, this.postId})
-      : this._likesService = LikesService(uid: uid) {
-    _isLiked = initialIsLiked;
-    _noLikes = initialNoLikes;
-  }
+//   LikesModel(bool initialIsLiked, int initialNoLikes, {this.uid, this.postId})
+//       : this._likesService = LikesService(uid: uid) {
+//     _isLiked = initialIsLiked;
+//     _noLikes = initialNoLikes;
+//   }
 
-  int get noLikes => _noLikes;
-  bool get isLiked => _isLiked;
+//   int get noLikes => _noLikes;
+//   bool get isLiked => _isLiked;
 
-  void likePressed() {
-    if (_isLiked) {
-      _likesService.unlikePost(postId);
-      _noLikes -= 1;
-      _isLiked = false;
-    } else {
-      _likesService.likePost(postId);
-      _noLikes += 1;
-      _isLiked = true;
-    }
-    notifyListeners();
-  }
+//   void likePressed() {
+//     if (_isLiked) {
+//       _likesService.unlikePost(postId);
+//       _noLikes -= 1;
+//       _isLiked = false;
+//     } else {
+//       _likesService.likePost(postId);
+//       _noLikes += 1;
+//       _isLiked = true;
+//     }
+//     notifyListeners();
+//   }
 
-  void manuallySetState(int noLikes, bool isLiked) {
-    _noLikes = noLikes;
-    _isLiked = isLiked;
-    notifyListeners();
-  }
-}
+//   void manuallySetState(int noLikes, bool isLiked) {
+//     _noLikes = noLikes;
+//     _isLiked = isLiked;
+//     notifyListeners();
+//   }
+// }
 
-class LikeBar extends StatelessWidget {
-  //the widget will listen to a likesModel to interact with the state. It will be rebuilt everytime the state changes in LikesModel.
-  //must be placed in below a LikesModel in widget tree
-  @override
-  Widget build(BuildContext context) {
-    var likesModel = Provider.of<LikesModel>(context);
-    return FlatButton(
-        onPressed: () {
-          likesModel.likePressed();
-        },
-        child: Container(
-            height: 40,
-            child: Row(children: <Widget>[
-              Expanded(
-                  child: Row(children: [
-                Container(
-                  width: 25,
-                  height: 25,
-                  padding: const EdgeInsets.all(0.0),
-                  child: (likesModel.isLiked)
-                      ? Image.asset('assets/images/like_selected.png',
-                          color: HexColor('ff6b6c'))
-                      : Image.asset('assets/images/like.png',
-                          color: Colors.grey[850]),
-                ),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.only(left: 10),
-                        child: Text(
-                          likesModel.noLikes != null
-                              ? likesModel.noLikes.toString()
-                              : "",
-                          style:
-                              TextStyle(fontSize: 15, color: Colors.grey[850]),
-                          textAlign: TextAlign.left,
-                        )))
-              ]))
-            ])));
-  }
-}
+// class LikeBar extends StatelessWidget {
+//   //the widget will listen to a likesModel to interact with the state. It will be rebuilt everytime the state changes in LikesModel.
+//   //must be placed in below a LikesModel in widget tree
+//   @override
+//   Widget build(BuildContext context) {
+//     var likesModel = Provider.of<LikesModel>(context);
+//     return FlatButton(
+//         onPressed: () {
+//           likesModel.likePressed();
+//         },
+//         child: Container(
+//             height: 40,
+//             child: Row(children: <Widget>[
+//               Expanded(
+//                   child: Row(children: [
+//                 Container(
+//                   width: 25,
+//                   height: 25,
+//                   padding: const EdgeInsets.all(0.0),
+//                   child: (likesModel.isLiked)
+//                       ? Image.asset('assets/images/like_selected.png',
+//                           color: HexColor('ff6b6c'))
+//                       : Image.asset('assets/images/like.png',
+//                           color: Colors.grey[850]),
+//                 ),
+//                 Expanded(
+//                     child: Container(
+//                         margin: EdgeInsets.only(left: 10),
+//                         child: Text(
+//                           likesModel.noLikes != null
+//                               ? likesModel.noLikes.toString()
+//                               : "",
+//                           style:
+//                               TextStyle(fontSize: 15, color: Colors.grey[850]),
+//                           textAlign: TextAlign.left,
+//                         )))
+//               ]))
+//             ])));
+//   }
+// }
 
 class NewLikeButton extends StatefulWidget {
   //LOOk UP WHERE STATE IS ACTUALLY KEPT IN THESE WIDGETS
