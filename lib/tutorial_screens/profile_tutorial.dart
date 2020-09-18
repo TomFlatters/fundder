3b8903f4433872a6c12de69e66a832f1fd5b1807
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-09-19 00:20:48 +0100

    import fixed

--- lib/tutorial_screens/profile_tutorial.dart
+++ lib/tutorial_screens/profile_tutorial.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fundder/tutorial_screens/Profile/profile_1.dart';
 import 'tutorial_screen_controller.dart';
-import 'profile/profile_1.dart';
+
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
