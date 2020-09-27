5b29106348977ea5e3231cf941e9b0675ea14265
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-09-27 21:38:52 +0100

    bottom sheet now 3/4 height

--- lib/privacyIcon.dart
+++ lib/privacyIcon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fundder/SearchSelectFollowers.dart';
 import 'package:fundder/helper_classes.dart';
 
 class PrivacyIcon extends StatefulWidget {
@@ -47,16 +48,28 @@ class SelectedFollowersOnlyPrivacyToggle extends StatelessWidget {
       leading: Icon(Icons.lock_outline),
       title: Text('Private Mode'),
       subtitle: Text(
-          "Choose specific people from your followers that you'd like to see this post"),
+          "Choose specific people from your followers that you'd like to allow to see this post"),
       onTap: () {
         Navigator.of(context).pop();
         print("SelectedFollowersOnlyPrivacyToggle selected");
         showModalBottomSheet(
+            isScrollControlled: true,
             context: context,
             builder: (context) {
               var height = MediaQuery.of(context).size.height;
               return Container(
                 height: 0.75 * height,
+                color: Color(0xFF737373),
+                child: Container(
+                  child: SearchSelectFollowers(),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).canvasColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(10),
+                      topRight: const Radius.circular(10),
+                    ),
+                  ),
+                ),
               );
             });
       },
