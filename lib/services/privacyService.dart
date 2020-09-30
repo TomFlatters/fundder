270fc6d759593acacb8398a07a464272a758d7d1
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-09-30 19:12:28 +0100

    skeleton code filled. Functioning privacy service

--- lib/services/privacyService.dart
+++ lib/services/privacyService.dart
@@ -28,22 +28,43 @@ class PrivacyService {
 
 class PostPrivacyToggle {
   final String postId;
+  final CollectionReference _postsCollection =
+      Firestore.instance.collection('postsV2');
   PostPrivacyToggle(this.postId);
   /**make this post private and available to all followers*/
   Future makePrivate() async {
-    //call cloud function in the backend
+    await _postsCollection
+        .document(postId)
+        .setData({'isPrivate': true}, merge: true);
     return;
   }
 
   /**Makes the post private and available only to specific followers */
-  Future makeAvailableToSpecificPeople(List<String> uids) async {
-    //call cloud function in the backend
+  Future makeAvailableToSpecificPeople(List uids) async {
+    await _postsCollection.document(postId).setData(
+        {'isPrivate': true, 'selectedPrivateViewers': uids},
+        merge: true);
     return;
   }
 
 /**Makes a private post public.*/
   Future makePostPublic() async {
-    //call cloud function in the backend
+    //make the post public and remove selected followers
+    await _postsCollection.document(postId).setData(
+        {'isPrivate': false, 'selectedPrivateViewers': FieldValue.delete()},
+        merge: true);
     return;
+  }
+
+/**
+ * Checks the private status of this post and returns the selected private
+ * viewers if they exist.
+*/
+  Future<Map> isPrivate() async {
+    Map res = {};
+    var postDoc = await _postsCollection.document(postId).get();
+    res["isPrivate"] = postDoc.data["isPrivate"] == true;
+    res["selectedViewers"] = postDoc.data['selectedPrivateViewers'];
+    return res;
   }
 }
