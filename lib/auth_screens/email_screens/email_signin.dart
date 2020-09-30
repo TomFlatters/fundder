5a5b6a8c3e9bfb01f8657921491568cdc7429589
adrikoz <adrikozh@gmail.com>
2020-09-30 12:01:34 +0100

    fixed overflow error on login screens

--- lib/auth_screens/email_screens/email_signin.dart
+++ lib/auth_screens/email_screens/email_signin.dart
@@ -27,169 +27,172 @@ class _EmailSignInState extends State<EmailSignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loading
-          ? Loading()
-          : Column(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(top: 50),
-                    height: 40,
-                    child: Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: widget.previousPage,
+        body: loading
+            ? Loading()
+            : SingleChildScrollView(
+                child: Container(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 50),
+                        height: 40,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: widget.previousPage,
+                            ),
+                            Expanded(
+                                child: Align(
+                              alignment: Alignment.centerRight,
+                              child: FlatButton(
+                                  onPressed: widget.nextPage,
+                                  child: Text('Register')),
+                            ))
+                          ],
+                        )),
+                    Expanded(
+                      child: Center(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 10),
+                          child: Text('Log in with email',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 30)),
                         ),
-                        Expanded(
-                            child: Align(
-                          alignment: Alignment.centerRight,
-                          child: FlatButton(
-                              onPressed: widget.nextPage,
-                              child: Text('Register')),
-                        ))
-                      ],
-                    )),
-                Expanded(
-                  child: Center(
-                    child: Container(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                      child: Text('Log in with email',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 30)),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-                    child: Form(
-                        key: _signinKey,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              TextFormField(
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.deny(
-                                      RegExp("[ ]"))
-                                ],
-                                initialValue: email,
-                                decoration: textInputDecoration.copyWith(
-                                    hintText: 'Email'),
-                                validator: (val) =>
-                                    val.isEmpty ? 'Enter an email' : null,
-                                onChanged: (val) {
-                                  if (mounted)
-                                    setState(() {
-                                      email = val;
-                                    });
-                                },
-                              ),
-                              SizedBox(height: 20.0),
-                              TextFormField(
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.deny(
-                                      RegExp("[ ]"))
-                                ],
-                                decoration: textInputDecoration.copyWith(
-                                    hintText: 'Password'),
-                                validator: (val) => val.length < 6
-                                    ? 'Enter a password longer than 5 characters'
-                                    : null,
-                                obscureText: true,
-                                onChanged: (val) {
-                                  if (mounted)
-                                    setState(() {
-                                      password = val;
-                                    });
-                                },
-                              ),
-                              SizedBox(height: 20.0),
-                              GestureDetector(
-                                onTap: () async {
-                                  if (email != '') {
-                                    signinerror =
-                                        await widget.auth.forgotPassword(email);
-                                    DialogManager().createDialog(
-                                        'Error', signinerror, context);
-                                    if (mounted) setState(() {});
-                                  } else {
-                                    if (mounted)
-                                      setState(() {
-                                        signinerror =
-                                            'Please enter your email for us to send a reset link';
-                                      });
-                                    DialogManager().createDialog(
-                                        'Error', signinerror, context);
-                                  }
-                                },
-                                child: Text(
-                                  'Forgot Password?',
-                                  style: TextStyle(
-                                      color: HexColor('ff6b6c'),
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              SizedBox(height: 20.0),
-                              Padding(
-                                padding: EdgeInsets.symmetric(vertical: 0),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(vertical: 0),
-                              )
-                            ]))),
-                PrimaryFundderButton(
-                  text: 'Log In',
-                  onPressed: () async {
-                    if (_signinKey.currentState.validate()) {
-                      if (mounted) setState(() => loading = true);
-                      String result = await widget.auth
-                          .signInWithEmailAndPassword(email, password);
-                      if (result == null) {
-                        if (mounted)
-                          setState(() {
-                            signinerror = 'Failed to sign in user';
-                            loading = false;
-                          });
-                        DialogManager()
-                            .createDialog('Error', signinerror, context);
-                      } else {
-                        if (result != 'Success') {
-                          if (mounted) {
+                    Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 30.0),
+                        child: Form(
+                            key: _signinKey,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  TextFormField(
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.deny(
+                                          RegExp("[ ]"))
+                                    ],
+                                    initialValue: email,
+                                    decoration: textInputDecoration.copyWith(
+                                        hintText: 'Email'),
+                                    validator: (val) =>
+                                        val.isEmpty ? 'Enter an email' : null,
+                                    onChanged: (val) {
+                                      if (mounted)
+                                        setState(() {
+                                          email = val;
+                                        });
+                                    },
+                                  ),
+                                  SizedBox(height: 20.0),
+                                  TextFormField(
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.deny(
+                                          RegExp("[ ]"))
+                                    ],
+                                    decoration: textInputDecoration.copyWith(
+                                        hintText: 'Password'),
+                                    validator: (val) => val.length < 6
+                                        ? 'Enter a password longer than 5 characters'
+                                        : null,
+                                    obscureText: true,
+                                    onChanged: (val) {
+                                      if (mounted)
+                                        setState(() {
+                                          password = val;
+                                        });
+                                    },
+                                  ),
+                                  SizedBox(height: 20.0),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      if (email != '') {
+                                        signinerror = await widget.auth
+                                            .forgotPassword(email);
+                                        DialogManager().createDialog(
+                                            'Error', signinerror, context);
+                                        if (mounted) setState(() {});
+                                      } else {
+                                        if (mounted)
+                                          setState(() {
+                                            signinerror =
+                                                'Please enter your email for us to send a reset link';
+                                          });
+                                        DialogManager().createDialog(
+                                            'Error', signinerror, context);
+                                      }
+                                    },
+                                    child: Text(
+                                      'Forgot Password?',
+                                      style: TextStyle(
+                                          color: HexColor('ff6b6c'),
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20.0),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 0),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 0),
+                                  )
+                                ]))),
+                    PrimaryFundderButton(
+                      text: 'Log In',
+                      onPressed: () async {
+                        if (_signinKey.currentState.validate()) {
+                          if (mounted) setState(() => loading = true);
+                          String result = await widget.auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
                             if (mounted)
                               setState(() {
-                                signinerror = result;
+                                signinerror = 'Failed to sign in user';
                                 loading = false;
                               });
                             DialogManager()
                                 .createDialog('Error', signinerror, context);
+                          } else {
+                            if (result != 'Success') {
+                              if (mounted) {
+                                if (mounted)
+                                  setState(() {
+                                    signinerror = result;
+                                    loading = false;
+                                  });
+                                DialogManager().createDialog(
+                                    'Error', signinerror, context);
+                              }
+                            }
                           }
                         }
-                      }
-                    }
-                  },
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    GestureDetector(
+                        onTap: widget.nextPage,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have an account? ",
+                                style: TextStyle(color: Colors.grey[800]),
+                              ),
+                              Text(
+                                "Register",
+                                style: TextStyle(
+                                    color: HexColor('ff6b6c'),
+                                    fontWeight: FontWeight.w600),
+                              )
+                            ])),
+                    SizedBox(height: 25.0),
+                  ],
                 ),
-                SizedBox(height: 20.0),
-                GestureDetector(
-                    onTap: widget.nextPage,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Don't have an account? ",
-                            style: TextStyle(color: Colors.grey[800]),
-                          ),
-                          Text(
-                            "Register",
-                            style: TextStyle(
-                                color: HexColor('ff6b6c'),
-                                fontWeight: FontWeight.w600),
-                          )
-                        ])),
-                SizedBox(height: 25.0),
-              ],
-            ),
-    );
+              )));
   }
 }
