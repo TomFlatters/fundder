7af6a318b6f54bbdf4ff44de5ce03bd33bddf463
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2020-10-26 16:37:20 +0000

    no legitamate changes

--- lib/post_creation_widgets/share_post_controller.dart
+++ lib/post_creation_widgets/share_post_controller.dart
@@ -140,9 +140,7 @@ class _SharePostControllerState extends State<SharePostController> {
           description: 'Help support this fundraiser!',
           imageUrl: post.videoThumbnail != null
               ? Uri.parse(post.videoThumbnail)
-              : post.imageUrl != null
-                  ? Uri.parse(post.imageUrl)
-                  : null),
+              : post.imageUrl != null ? Uri.parse(post.imageUrl) : null),
     );
 
     final ShortDynamicLink shortDynamicLink = await parameters.buildShortLink();
