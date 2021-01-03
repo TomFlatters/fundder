b139cb750cffb78a60569d3e86a9957831335855
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2021-01-03 15:30:58 +0000

    money input field created

---
+++ lib/post_creation_widgets/input_field_widgets/money_input.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:fundder/helper_classes.dart';
+import 'package:fundder/post_creation_widgets/input_field_widgets/input_field_interface.dart';
+import 'package:fundder/shared/constants.dart';
+
+class MoneyInputField extends InputField {
+  String _targetAmount = "0.00";
+
+  bool get isInputValid {
+    return double.parse(_targetAmount) >= 2.00;
+  }
+
+  StatefulWidget buildWidget() {
+    return MoneyInputBox(
+      onMoneyChanged: this.onMoneyChanged,
+    );
+  }
+
+  void onMoneyChanged(String newAmount) {
+    this._targetAmount = newAmount;
+    print("The validity status of money input field is: $isInputValid");
+  }
+}
+
+class MoneyInputBox extends StatefulWidget {
+  Function onMoneyChanged;
+  MoneyInputBox({@required this.onMoneyChanged});
+  @override
+  _MoneyInputBoxState createState() => _MoneyInputBoxState();
+}
+
+class _MoneyInputBoxState extends State<MoneyInputBox> {
+  MoneyMaskedTextController moneyController;
+
+  @override
+  void initState() {
+    super.initState();
+    moneyController = MoneyMaskedTextController(
+        decimalSeparator: '.', thousandSeparator: ',');
+
+    moneyController.afterChange = (String masked, double raw) {
+      widget.onMoneyChanged(masked);
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text('My target amount: ',
+          style: TextStyle(
+            fontFamily: 'Founders Grotesk',
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          )),
+      Text(
+        '£',
+        style: TextStyle(
+            fontWeight: FontWeight.w100,
+            fontFamily: 'Founders Grotesk',
+            fontSize: 18,
+            color: HexColor('ff6b6c')),
+      ),
+      Expanded(
+        child: TextField(
+          keyboardType: TextInputType.number,
+          style: TextStyle(
+            fontWeight: FontWeight.w100,
+            fontFamily: 'Founders Grotesk',
+            fontSize: 18,
+            color: Colors.black,
+          ),
+          controller: moneyController,
+          decoration: textInputDecoration.copyWith(hintText: 'Amount in £'),
+        ),
+      )
+    ]);
+  }
+}
