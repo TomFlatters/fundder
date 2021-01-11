5c79ca360847b6d81fa3917a253558ae36abbdb4
Sarthak Jain <sarthakjain.qwerty@gmail.com>
2021-01-11 23:13:19 +0000

    added video controller and made stateful for that

--- lib/post_creation_widgets/creation_tiles/post_preview.dart
+++ lib/post_creation_widgets/creation_tiles/post_preview.dart
@@ -5,9 +5,14 @@ import 'package:fundder/post_widgets/postBody.dart';
 import 'package:fundder/post_widgets/postHeader.dart';
 import 'package:fundder/models/post.dart';
 import 'package:fundder/models/charity.dart';
+import 'package:fundder/shared/loading.dart';
 import 'package:provider/provider.dart';
+import 'package:fundder/post_creation_widgets/input_field_widgets/media_upload.dart';
+import 'package:video_player/video_player.dart';
 
-class PostPreview extends StatelessWidget {
+//Technically passing all these parameters to do with state is redundant now
+// but this has been kept as legacy to save time in getting version 3.0.0 out.
+class PostPreview extends StatefulWidget {
   final Charity charity;
   final String authorUid;
   final String authorUsername;
@@ -28,8 +33,24 @@ class PostPreview extends StatelessWidget {
       this.targetAmount,
       this.hashtags});
   @override
+  _PostPreviewState createState() => _PostPreviewState();
+}
+
+class _PostPreviewState extends State<PostPreview> {
+  Function _disposeVideoController = () {};
+  @override
+  void dispose() {
+    _disposeVideoController();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final mediaState = Provider.of<MediaStateManager>(context, listen: false);
+    final height = (MediaQuery.of(context).size.height) * 2 / 5;
+    final width = MediaQuery.of(context).size.width;
     final user = Provider.of<User>(context);
+    this._disposeVideoController = mediaState.removeVideoPlayer;
     return ListView(children: <Widget>[
       Container(
         color: Colors.grey[200],
@@ -48,7 +69,7 @@ class PostPreview extends StatelessWidget {
                 margin: EdgeInsets.only(bottom: 10),
                 padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
                 child: Text(
-                  selected == 0
+                  widget.selected == 0
                       ? 'This is what your post would look like:'
                       : 'This is what your challenge, once somebody accepts it, would look like:',
                   style: TextStyle(
@@ -59,24 +80,53 @@ class PostPreview extends StatelessWidget {
                 ),
               ),
               PostHeader(
-                  postAuthorId: authorUid,
-                  postAuthorUserName: authorUsername,
-                  targetCharity: charity.id,
-                  charityLogo: charity.image),
-              Container(
-                child: SizedBox(child: imageView),
-                margin: EdgeInsets.only(bottom: 10.0),
-              ),
+                  postAuthorId: widget.authorUid,
+                  postAuthorUserName: widget.authorUsername,
+                  targetCharity: widget.charity.id,
+                  charityLogo: widget.charity.image),
+              (mediaState.hasVideo)
+                  ? FutureBuilder(
+                      future: mediaState.videoController.initialize(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          mediaState.videoController.play();
+                          mediaState.videoController.setVolume(0);
+                          mediaState.videoController.setLooping(true);
+                          return AspectRatio(
+                              child: VideoPlayer(mediaState.videoController),
+                              aspectRatio:
+                                  mediaState.videoController.value.aspectRatio);
+                        } else {
+                          return Container(
+                              child: Loading(),
+                              constraints: BoxConstraints(
+                                minWidth: width,
+                                minHeight: height,
+                              ),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ));
+                        }
+                      },
+                    )
+                  : Container(
+                      child: SizedBox(child: widget.imageView),
+                      margin: EdgeInsets.only(bottom: 10.0),
+                    ),
               PostBody(
                 likesManager: null,
                 maxLines: 99999999,
                 postData: Post(
                     isPrivate: user.isPrivate,
-                    title: title,
-                    subtitle: subtitle,
-                    hashtags: hashtags,
+                    title: widget.title,
+                    subtitle: widget.subtitle,
+                    hashtags: widget.hashtags,
                     moneyRaised: 0,
-                    targetAmount: selected != 1 ? targetAmount : '-1'),
+                    targetAmount:
+                        widget.selected != 1 ? widget.targetAmount : '-1'),
               )
             ],
           ),
